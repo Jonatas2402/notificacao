69df4c38b2a7153f6f16b5589ba6d45ff0417aae
jonatas <jonatas_dev@hotmail.com>
2026-04-06 12:23:10 -0300

    Adicionado métodos de envio de email para notificar o i=usuário

---
+++ src/main/java/com/projeto/notificacao/business/EmailService.java
@@ -0,0 +1,60 @@
+package com.projeto.notificacao.business;
+
+import com.projeto.notificacao.business.dto.TarefaDTO;
+import com.projeto.notificacao.infrastructure.exceptions.EmailException;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender javaMailSender;
+    private final TemplateEngine templateEngine;
+
+
+    @Value("${envio.email.remetente}")
+    public String remetente;
+
+    @Value("${envio.email.nomeRemetente}")
+    private String nomeRemetente;
+
+    /*Método que envia email.*/
+    public void enviaEmail(TarefaDTO dto){
+        if (dto.getEmailUsuario() == null || dto.getEmailUsuario().isBlank()) {
+            throw new EmailException("O endereço de email do usuário não pode ser nulo ou vazio");
+        }
+        try{
+            MimeMessage mensagem = javaMailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mensagem, true, StandardCharsets.UTF_8.name());
+
+            mimeMessageHelper.setFrom(new InternetAddress(remetente, nomeRemetente));
+
+            mimeMessageHelper.setTo(InternetAddress.parse(dto.getEmailUsuario()));
+
+            mimeMessageHelper.setSubject("Notificação de tarefa");
+
+           Context context = new Context();
+           context.setVariable("NomeTarefa", dto.getNomeTarefa());
+           context.setVariable("dataEvento", dto.getDataEvento());
+           context.setVariable("descricao", dto.getDescricao());
+
+           String template = templateEngine.process("Notificacao", context);
+           mimeMessageHelper.setText(template,true);
+           javaMailSender.send(mensagem);
+        }catch (MessagingException | UnsupportedEncodingException e) {
+            throw new EmailException("Erro ao enviar o email ", e.getCause());
+        }
+    }
+
+}
